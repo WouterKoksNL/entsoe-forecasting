8119f53eb0bec65722e49a68417142fe429e3ebb
WouterKoksNL <wouterkoks135@live.nl>
2025-06-06 12:46:20 +0200

    Create utils.py

---
+++ src/utils.py
@@ -0,0 +1,14 @@
+import os 
+import pandas as pd
+
+def make_dir(path):
+    """
+    Create a directory if it does not exist.
+    """
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def convert_index_to_datetime(ser):
+    ser.index = pd.to_datetime(ser.index, utc=True)
+    return ser
