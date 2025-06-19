4f00681b5ebfa501f85a15ebe3fdf757b0782c43
WouterKoksNL <wouterkoks135@live.nl>
2025-06-19 12:55:56 +0200

    split analysis into forecasting and fitting

---
+++ src/fitting.py
@@ -0,0 +1,107 @@
+
+import pickle
+import pandas as pd
+import numpy as np
+from scipy.optimize import curve_fit
+from sklearn.metrics import r2_score, root_mean_squared_error
+from typing import Callable
+
+from .utils import make_dir
+from .forecast_data_handling import get_data
+
+
+def get_fitting_function(fitting_function_name: str) -> Callable:
+    """
+    Get the fitting function based on the provided name.
+
+    Args:
+        fitting_function_name (str): Name of the fitting function to be used.
+
+    Returns:
+        Callable: The fitting function.
+    """
+    if fitting_function_name == "asymptotic":
+        return lambda x, a, b: a * x / (x + b)
+    else:
+        raise NotImplementedError(f"Fitting function '{fitting_function_name}' is not implemented.")
+    
+
+def calc_rmse(
+        lead_time_range: range | list[int],
+        y_test_dict: dict[int, np.ndarray], 
+        y_pred_dict: dict[int, np.ndarray],
+        scaling_value: float,
+        ) -> pd.Series:
+    """Calculate RMSE for each lead time and return as a pandas Series.
+
+    Args:
+        lead_time_range (range | list[int]): _description_
+        y_test_dict (dict[int, np.ndarray]): _description_
+        y_pred_dict (dict[int, np.ndarray]): _description_
+        scaling_value (float): _description_
+
+    Returns:
+        pd.Series: _description_
+    """
+    rmse_ser = pd.Series(index=lead_time_range, dtype=float)
+    for lead_time in lead_time_range:
+        y_test = y_test_dict[lead_time]
+        y_pred = y_pred_dict[lead_time]
+        rmse_ser.loc[lead_time] = root_mean_squared_error(y_test, y_pred) / scaling_value
+    return rmse_ser
+
+
+def calc_fitting_params(
+    lead_time_range,
+    rmse_ser: pd.Series,
+    fitting_function: Callable,
+    ) -> tuple[float, float, float]:
+
+    asymptotic_params, _ = curve_fit(fitting_function, lead_time_range, rmse_ser)
+    a, b = asymptotic_params  # hard-coded two fitting parameters! 
+    r_squared = r2_score(rmse_ser, fitting_function(lead_time_range, *asymptotic_params))
+    return a, b, r_squared 
+
+
+def calc_fitting_params_loop(
+        config,
+    ):
+    """Calculate RMSE w.r.t. lead time for each zone and fit the asymptotic function to the RMSE values.
+
+    Args:
+        config (ConfigSettings): Configuration settings 
+    """
+    output_param_dir = f"output/{config.run_id}"
+    forecast_pickle_dir = f"data/pickled_forecasts/{config.run_id}"
+    make_dir(output_param_dir)
+
+    lead_time_range = range(1, config.max_lead_time + 1)
+    fitting_function = get_fitting_function(config.fitting_function)
+
+    rmse_dict = {}
+    params_dict = {}
+    for error_type in config.error_types:
+        rmse_dict[error_type] = {}
+        zones = config.zones_error_types[error_type]
+        param_df = pd.DataFrame(index=zones, columns=['a', 'b'])
+        for zone in zones:
+            y_test_dict, y_pred_dict, scaling_value, _, _ = get_data(
+                forecast_pickle_dir, zone, error_type)
+            
+
+            rmse_ser = calc_rmse(lead_time_range, y_test_dict, y_pred_dict, scaling_value)
+            a, b, r_squared = calc_fitting_params(lead_time_range, rmse_ser, fitting_function)
+            rmse_dict[error_type][zone] = rmse_ser
+            param_df.loc[zone, 'a'] = a
+            param_df.loc[zone, 'b'] = b
+            param_df.loc[zone, 'r_squared'] = r_squared
+            param_df.to_csv(f"{output_param_dir}/rmse_params.csv", index=True)
+        params_dict[error_type] = param_df
+        
+    # pickle rmse values
+    with open(f"{output_param_dir}/rmse_dict.pkl", "wb") as f:
+        pickle.dump(rmse_dict, f)
+
+    return params_dict, rmse_dict
+
+
---
+++ src/forecasting.py
@@ -0,0 +1,124 @@
+import pandas as pd
+import numpy as np
+
+import pickle
+from typing import Callable
+
+from .load_saved_data import (
+    load_load_data,
+    load_generation_data,
+)
+from .utils import make_dir
+from .load_config import ConfigSettings
+from .algorithms.get_algorithm import get_algorithm
+from .forecast_data_handling import save_data
+
+
+
+
+def get_forecasts(
+        error_type: str, 
+        zone: str, 
+        years: list[int], 
+        train_and_test_function: Callable, 
+        n_lags=3, 
+        lead_time_range: range | list[int] = range(1, 13),
+        data_folder: str = "data/input_entsoe",
+        train_test_split: float = 0.8,
+        pickle_output_flag: bool = True,
+        forecast_pickle_dir: str = "data/pickled_forecasts",
+        ) -> tuple[dict[int, np.ndarray], dict[int, np.ndarray]]:
+    """
+    Calculate RMSE w.r.t. lead time for each zone and fit the asymptotic function to the RMSE values. 
+    Returns the test values and predictions for each lead time.
+    Optionally saves the results as pickle files.
+
+    Args:
+        error_type (str): Type of error to forecast, e.g., 'load', 'Wind Onshore', 'Solar'.
+        zone (str): Zone for which to calculate the forecasts. Must conform with ENTSO-E convention. 
+        years (list[int]): List of years for which to calculate the forecasts.
+        train_and_test_function (Callable): Function to train and test the model (in src.algorithms)
+        n_lags (int, optional): Number of lags to use in the model. Defaults to 3.
+        lead_time_range (range | list[int], optional): Range of lead times to consider. Defaults to range(1, 13).
+        data_folder (str, optional): Folder where input data is stored. Defaults to "data/input_entsoe".
+        train_test_split (float, optional): Proportion of data to use for training. Defaults to 0.8.
+        pickle_output_flag (bool, optional): Whether to save the results as pickle files. Defaults to True.
+        forecast_pickle_dir (str, optional): Directory where pickle files will be saved. Defaults to "data/pickled_forecasts".
+
+
+    """
+    y_test_dict = {}
+    y_pred_dict = {}
+
+
+    print(f"Processing error type {error_type}...")
+
+    if error_type == "load":
+        target_series, forecast_series, scaling_value = load_load_data(years, zone, folder=data_folder)
+    else:
+        target_series, forecast_series, scaling_value = load_generation_data(years, zone, folder=data_folder, carrier=error_type)
+    if target_series.isna().sum() > 10:
+        raise ValueError(f"Target series for {zone} in {error_type} has too many NaN values. Please check the data.")
+    target_series.ffill(inplace=True)
+    forecast_series.ffill(inplace=True)
+    target_series_mean = target_series.mean()
+    target_series_std = target_series.std()
+    forecast_series_mean = forecast_series.mean()
+    forecast_series_std = forecast_series.std()
+    difference = target_series - forecast_series
+    difference_mean = difference.mean()
+    difference_std = difference.std()
+    # Normalize the series
+    target_series = (target_series - target_series_mean) / target_series_std
+    forecast_series = (forecast_series - forecast_series_mean) / forecast_series_std
+    difference = (difference - difference_mean) / difference_std
+
+    target_series_hour_sin = np.sin(target_series.index.hour * (2 * np.pi / 24))
+    target_series_series_dayofyear_sin = np.sin(target_series.index.dayofyear * (2 * np.pi / 365))
+    for n_lead_time in lead_time_range:     
+        reduced_forecast_error_usability_entry = np.maximum(0, n_lags - target_series.index.hour)
+        y_test, y_pred = train_and_test_function(
+            target_series.values, 
+            forecast_series.values, 
+            difference.values, 
+            reduced_forecast_error_usability_entry, 
+            target_series_hour_sin, 
+            target_series_series_dayofyear_sin, 
+            n_lags=n_lags, 
+            n_lead_time=n_lead_time,
+            train_test_split=train_test_split,
+            )
+        y_test_dict[n_lead_time] = y_test * target_series_std + target_series_mean
+        y_pred_dict[n_lead_time] = y_pred * target_series_std + target_series_mean
+    
+    make_dir(f"{forecast_pickle_dir}/{zone}/{error_type}")
+    if pickle_output_flag:
+        save_data(forecast_pickle_dir, zone, error_type, y_test_dict, y_pred_dict, scaling_value, target_series, forecast_series)   
+
+
+    return y_test_dict, y_pred_dict
+
+
+def get_forecast_loop(
+        config: ConfigSettings, 
+        ):
+    """_summary_
+
+    Args:
+        config (ConfigSettings): _description_
+    """
+    forecasting_function = get_algorithm(config.forecasting_model)
+    for error_type in config.error_types:
+        zones = config.zones_error_types[error_type]
+        for zone in zones:
+            get_forecasts(
+                error_type, 
+                zone, 
+                config.years, 
+                train_and_test_function=forecasting_function, 
+                n_lags=config.n_lags, 
+                lead_time_range=range(1, config.max_lead_time + 1),
+                train_test_split=config.train_test_split,
+                forecast_pickle_dir=f"data/pickled_forecasts/{config.run_id}",
+                )
+    return 
