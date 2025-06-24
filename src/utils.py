cf80ef3e42c31ac37b015e7112b2a4aef97f0a38
WouterKoksNL <wouterkoks135@live.nl>
2025-06-24 14:30:47 +0200

    extend get_hourly functionality

--- src/utils.py
+++ src/utils.py
@@ -13,5 +13,11 @@ def convert_index_to_datetime(ser):
     ser.index = pd.to_datetime(ser.index, utc=True)
     return ser
 
-def get_hourly(series):
-    return series.loc[series.index.minute == 0]
+def get_hourly(ser, set_hour_indices=True):
+    selection = ser.loc[ser.index.minute == 0]
+    if set_hour_indices:
+        selection.index = selection.index.hour
+    return selection
+
+def select_day(ser, year, month, day):
+    return ser.loc[(ser.index.year == year) & (ser.index.month == month) & (ser.index.day == day)]
